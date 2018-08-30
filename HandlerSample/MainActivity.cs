df2264804a303608f8b4c11b6a6e01387af492a3
SaitouRedEyes <zeca.saitou@gmail.com>
2018-08-29 22:15:01 -0300

    Handler example on Xamarin.Android
    This is a example of Handler use on Xamarin.Android.

---
+++ HandlerSample/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Java.Lang;
+
+namespace HandlerSample
+{
+    [Activity(Label = "HandlerSample", MainLauncher = true)]
+    public class MainActivity : Activity, IRunnable
+    {
+        private EditText editTextN1;
+        private EditText editTextN2;
+        private float result;
+        private TextView textViewResult;
+        private Handler handler;
+        private Bundle bundle;
+        private Message message;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            handler = ProcessScript.GetInstance(Window.DecorView.RootView);
+            bundle = new Bundle();
+
+            Button buttonSum = (Button)FindViewById(Resource.Id.buttonSum);
+            editTextN1 = (EditText)FindViewById(Resource.Id.editTextN1);
+            editTextN2 = (EditText)FindViewById(Resource.Id.editTextN2);
+
+            buttonSum.Click += OnButtonSumClicked;
+        }
+
+        private void OnButtonSumClicked(object sender, EventArgs e)
+        {
+            if (!editTextN1.Text.Equals("") && !editTextN2.Text.Equals(""))
+            {
+                result = float.Parse(editTextN1.Text) + float.Parse(editTextN2.Text);
+
+                new Thread(this).Start();
+            }
+            else
+            {
+                Toast.MakeText(this, "Campos vazios!", ToastLength.Short).Show();
+            }
+        }
+
+        public void Run()
+        {
+            //android.view.ViewRoot$CalledFromWrongThreadException:
+            //only the original thread that created a view hierarchy can touch its views.
+            textViewResult = (TextView)FindViewById(Resource.Id.TextViewResult);
+            textViewResult.Text = "SOMA: " + result;
+
+            //Solution is use Handler to send a message
+            //to the main thread message queue tp update the view
+            //bundle.PutString("OP", result.ToString());
+            //message = new Message();
+            //message.Data = bundle;
+
+            //send message
+            //handler.SendMessage(message);
+
+            //send message with delay
+            //handler.SendMessageDelayed(message, 5000);
+        }
+    }
+}
+
---
+++ HandlerSample/ProcessScript.cs
@@ -0,0 +1,34 @@
+﻿using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace HandlerSample
+{
+    class ProcessScript : Handler
+    {
+        private static ProcessScript instance;
+        private TextView textViewResult;
+
+        public ProcessScript(View v)
+        {
+            textViewResult = (TextView)v.FindViewById(Resource.Id.TextViewResult);
+        }
+
+        public static ProcessScript GetInstance(View v)
+        {
+            if(instance == null)
+            {
+                instance = new ProcessScript(v);
+            }
+
+            return instance;
+        }
+
+        public override void HandleMessage(Message msg)
+        {
+            base.HandleMessage(msg);
+
+            textViewResult.Text = "SOMA: " + msg.Data.GetString("OP");
+        }
+    }
+}
